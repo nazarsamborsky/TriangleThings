d33dffee497d0560aff46f82d87f12e35e083f62
Nazar <Nazar@DESKTOP-BUHHMEA>
2021-10-09 19:30:54 +0200

    dokonceny DU

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TriangleThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle cisloJedna = new Triangle(6); //rovnostranný trojúhelník
+            cisloJedna.A = 0; // strana nemuze byt mensi nez 0
+            Console.WriteLine(cisloJedna.A + " je délka strany A");
+            Console.WriteLine(cisloJedna.B + " je délka strany B");
+            Console.WriteLine(cisloJedna.C + " je délka strany C");
+            Console.WriteLine(cisloJedna.GetOutline() + " je obvod");
+            Console.WriteLine(cisloJedna.GetArea() + " je obsah");
+            Console.WriteLine("Trojuhelnik " + cisloJedna.IsExisting() + " existovat");
+            Console.WriteLine("Trojuhelnik " + Triangle.CanExist(6, 1, 4) + " existovat"); // static CanExsist
+            Console.WriteLine(cisloJedna.GetAlpha() + " je uhel alfa");
+            Console.WriteLine(cisloJedna.GetBeta() + " je uhel beta");
+            Console.WriteLine(cisloJedna.GetGamma() + " je uhel gamma");
+            Console.WriteLine();
+
+
+            Triangle cisloDva = new Triangle(3, 4, 5);
+            Console.WriteLine(cisloDva.A + " je délka strany A");
+            Console.WriteLine(cisloDva.B + " je délka strany B");
+            Console.WriteLine(cisloDva.C + " je délka strany C");
+            Console.WriteLine(cisloDva.GetOutline() + " je obvod");
+            Console.WriteLine(cisloDva.GetArea() + " je obsah");
+            Console.WriteLine("Trojuhelnik " + cisloDva.IsExisting() + " existovat");
+            Console.WriteLine(cisloDva.GetAlpha() + " je uhel alfa");
+            Console.WriteLine(cisloDva.GetBeta() + " je uhel beta");
+            Console.WriteLine(cisloDva.GetGamma() + " je uhel gamma");
+            Console.WriteLine();
+            cisloDva.SetSides(4, 8, 7);
+            // strany po zmeneni
+            Console.WriteLine(cisloDva.A + " je délka strany A");
+            Console.WriteLine(cisloDva.B + " je délka strany B");
+            Console.WriteLine(cisloDva.C + " je délka strany C");
+
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleThings
+{
+    class Triangle
+    {
+        private double _a;
+        private double _b;
+        private double _c;
+        private double alpha;
+        private double beta;
+        private double gamma;
+
+        public Triangle(double a, double b, double c)
+        {
+            if (a > 0 && b > 0 && c > 0)
+            {
+                _a = a;
+                _b = b;
+                _c = c;
+
+            }
+        }
+        public Triangle(double strana)
+        {
+            if (strana > 0)
+            {
+                _a = strana;
+                _b = strana;
+                _c = strana;
+            }
+        }
+
+        public double A
+        {
+            set { if(value > 0) _a = value;}
+            get { return _a;}
+        }
+
+        public double B
+        {
+            set { if(value > 0) _b = value;}
+            get { return _b;}
+        }
+
+        public double C
+        {
+            set { if(value > 0) _c = value;}
+            get { return _c;}
+        }
+
+        public void SetSides(double a, double b, double c)
+        {
+            if (a > 0 && b > 0 && c > 0)
+            {
+                _a = a;
+                _b = b;
+                _c = c;
+
+            }
+        }
+
+        public double GetArea()
+        {
+
+            double s = (_a + _b + _c) / 2;
+            double result = Math.Sqrt(s * (s - _a) * (s - _b) * s * (s - _c)); //obsah vsech trojuhelniku
+            result = Math.Round(result);
+            return result;
+        }
+
+ 
+        public double GetOutline()
+        {
+            double result = _a + _b +_c; //obvod pro vsechny trojuhelniky
+            return result;
+        }
+        public string IsExisting()
+        {
+            if (_a + _b > _c && _a + _c > _b && _b + _c > _a)
+            {
+                string result = "může";
+                return result;
+            }
+            else
+            {
+                string result = "nemůže";
+                return result;
+            }
+        }
+
+        public static string CanExist(double a, double b, double c)
+        {
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                string result = "může";
+                return result;
+            }
+            else
+            {
+                string result = "nemůže";
+                return result;
+            }
+        }
+        public double GetAlpha()
+        {
+            double s = (_a + _b + _c) / 2;
+            double alpha = Math.Asin(2 * Math.Sqrt(s * (s - _a) * (s - _b) * (s - _c)) / (_b * _c)) * (180 / Math.PI);
+            alpha = Math.Round(alpha);
+            return alpha;
+        }
+        public double GetBeta()
+        {
+            double s = (_a + _b + _c) / 2;
+            double beta = Math.Asin(2 * Math.Sqrt(s * (s - _a) * (s - _b) * (s - _c)) / (_a * _c)) * (180 / Math.PI);
+            beta = Math.Round(beta);
+            return beta;
+        }
+        public double GetGamma()
+        {
+            double s = (_a + _b + _c) / 2;
+            double gamma = Math.Asin(2 * Math.Sqrt(s * (s - _a) * (s - _b) * (s - _c)) / (_a * _b)) * (180/Math.PI);
+            gamma = Math.Round(gamma);
+            return gamma;
+        }
+    }    
+}
+
+
+
